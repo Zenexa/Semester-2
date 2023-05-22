c012583f9bbe4fe2715ed53735a2d7d907ce1afe
Yahaha Live user <kzenuar@gmail.com>
2023-05-23 01:03:27 +0700

    Sorting

---
+++ arraylist/latihan arraylist/arraylist/src/teuing.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;;
+
+class Mahasiswa {
+    Integer tata = 10;
+}
+
+public class teuing{
+    public static void main(String[] args) {
+        ArrayList <Mahasiswa> data = new ArrayList<Mahasiswa>();
+
+      
+
+
+
+
+    }
+}
